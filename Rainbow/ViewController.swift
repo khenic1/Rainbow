d4e54873acb31c38dfa3976c34ae11b4c6633c47
Kelley Henican <kelleyhenican2@gmail.com>
2020-01-21 20:42:11 -0800

    Initial commit

---
+++ Rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Kelley Henican on 1/21/20.
+//  Copyright © 2020 Kelley Henican. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
