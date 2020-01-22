6071e17b7c0c482c64160e207f2c2a8546613c3f
Kelley Henican <kelleyhenican2@gmail.com>
2020-01-21 21:34:50 -0800

    Set up initial views

---
+++ Rainbow/Storyboards/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by Kelley Henican on 1/21/20.
+//  Copyright © 2020 Kelley Henican. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}
---
+++ Rainbow/View Controllers/ColorDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorDetailViewController.swift
+//  Rainbow
+//
+//  Created by Kelley Henican on 1/21/20.
+//  Copyright © 2020 Kelley Henican. All rights reserved.
+//
+
+import UIKit
+
+class ColorDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
